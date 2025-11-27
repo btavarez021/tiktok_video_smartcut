b0ddb4981e331f155b98e1f33cd65d2f29074d59
Benny Tavarez <bennytavarez@hotmail.com>
2025-11-26 21:57:48 -0500

    add python version

--- utils_video.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def enforce_mp4(filename: str) -> str:
-    """
-    Ensures filename is basename only, lowercase, and ends with .mp4.
-    """
-    base = os.path.basename(filename).lower()
-    name, _ext = os.path.splitext(base)
-    return f"{name}.mp4"
