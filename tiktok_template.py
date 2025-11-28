320c9d83b261468c391607dabcf4938f7fc63bb6
Benny Tavarez <bennytavarez@hotmail.com>
2025-11-28 16:58:52 -0500

    fixing narration

--- tiktok_template.py
+++ tiktok_template.py
@@ -702,8 +702,8 @@ def edit_video(output_file: str = "output_tiktok_final.mp4", optimized: bool = F
         for idx, inp in enumerate(audio_inputs):
             delay_ms = int(round(inp["start"] * 1000))
             vol = inp["volume"]
-            # stereo-safe adelay
-            part = f"[{idx}:a]adelay={delay_ms}|{delay_ms},volume={vol}[a{idx}]"
+            part = f"[{idx}:a]adelay={delay_ms}:{delay_ms},volume={vol}[a{idx}]"
+
             filter_parts.append(part)
             labels.append(f"[a{idx}]")
 
